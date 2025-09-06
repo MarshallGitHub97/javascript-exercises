2889799d2b2d0da337bbb819f787ed067bbba2f3
Marcel <marcel97.ganske@gmail.com>
2025-09-06 15:55:38 +0200

    Master task 09 in js exercises

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
 
+    if (
+        (startNum == NaN || endNum == NaN) ||
+        (startNum < 0  || endNum < 0) ||
+        (!Number.isInteger(startNum) || !Number.isInteger(endNum))
+    ){
+        return "ERROR"
+    }
+    
+    if (startNum <= endNum) {    
+        for (let i = startNum; i <= endNum; i++) {
+          
+            sum = sum + i;
+         
+        }
+    } 
+    else if (startNum > endNum) {
+        for (let i = startNum; i >= endNum; i--){
+            sum = sum + i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
