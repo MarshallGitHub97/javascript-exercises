f446d39395497498954649760cad9589d12ac54c
Marcel <marcel97.ganske@gmail.com>
2025-09-06 14:06:26 +0200

    Master task 08 in js exercises

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, deleteFromArray) {
+    const indexOfDeletedElement = array.findIndex((element) => element == deleteFromArray )
+    array.splice(indexOfDeletedElement, 1)
+    return array
 };
 
 // Do not edit below this line
