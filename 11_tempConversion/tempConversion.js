a78add7950c60b4adaef1a171d132d4424fb0bcc
Marcel <marcel97.ganske@gmail.com>
2025-09-06 16:22:38 +0200

    Master task 11 in js exercises

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = 0;
+  celsius = (fahrenheit - 32) * (5 / 9)
+
+  return Number(celsius.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = 0;
+  fahrenheit = celsius * (9 / 5) + 32;
+
+  return Number(fahrenheit.toFixed(1))
 };
+
+console.log(convertToCelsius(32))
+console.log(convertToFahrenheit(38))
 
 // Do not edit below this line
 module.exports = {
