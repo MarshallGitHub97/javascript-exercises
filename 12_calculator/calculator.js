c6fcafa9ebcf7454cdb30daf728959a0a28785a2
Marcel <marcel97.ganske@gmail.com>
2025-09-15 20:26:23 +0200

    Master Task 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  arr.forEach((element) => sum += element)
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let sum = 1;
+  arr.forEach((element) => sum = sum * element)
+  return sum
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let power = 1
+  for (let i = 1; i <= b; i++) {
+    power = power * a
+  }
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let factorial = 1;
+  for(let i = 1; i < a; i++) {
+    factorial = factorial * (i + 1) 
+  }
+  return factorial;
 };
 
 // Do not edit below this line
