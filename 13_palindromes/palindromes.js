76ecfccd771728b5813627635769c26f1236c344
Marcel <marcel97.ganske@gmail.com>
2025-09-15 21:47:50 +0200

    Master Task 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let newStr = str.replaceAll(' ', '').replaceAll('.', '').replaceAll(',', '').replaceAll('!', '');
+    let array = newStr.toLowerCase().split('')
 
+    for (let i = 0; i < (array.length / 2); i++) {
+        let firstLetter = array.slice(i, i + 1);
+        let lastLetter = array.slice(array.length - i - 1, array.length - i);
+        
+        if (firstLetter.toString() !== lastLetter.toString()) {
+            return false
+        } 
+    }
+
+    return true
 };
 
 // Do not edit below this line
