7ad236a870a8897c6edf0b5657aec89d1d600094
Marcel <139146972+MarshallGitHub97@users.noreply.github.com>
2025-09-24 11:41:21 +0200

    Create Fibonacci Function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    let arr = [];
+    let arrValue = 0;
+    for(i = 0; i <= position; i++){
+        if(i == 0){
+            arrValue = 0;
+        } 
+        else if(i == 1) {
+            arrValue = 1;
+        }
+        else {
+            arrValue = arr[i-1] + arr[i]  
+        }
+        arr.push(arrValue);
+    }
+    return arr[position]
 };
 
 // Do not edit below this line
