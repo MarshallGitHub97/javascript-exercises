dd7883a1337d9d501c4e98d58e075fdd4ed97032
Marcel <139146972+MarshallGitHub97@users.noreply.github.com>
2025-09-25 13:31:24 +0200

    create the findTheOldest Function

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let oldestPerson = {}; 
+    let highestAge = 0:
+
+    people.forEach((element) => {
+        //When the Person is still alive get the actual year
+        const lastYear = element.yearOfDeath || new Date().getFullYear();
+        const ageOfPerson =  lastYear -  element.yearOfBirth;
+
+        if (highestAge > ageOfPerson) {
+            highestAge = ageOfPerson;
+            oldestPerson = element;
+        }
+
+    })
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
